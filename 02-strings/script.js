c803e0c016c99e82a286407e1a5a074214782450
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2025-12-19 16:37:07 +0100

    complete 02-strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,18 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+let name = 'Nicolò'
+console.log(name)
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 33
+let text = 'ciao'
+let bool = true
+console.log(typeof number)
+console.log(typeof text)
+console.log(typeof bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +21,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + ' ' + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length
 console.log(numberOfLettersInAlphabet); // Output: 26
